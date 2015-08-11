a7dc6a529789b088f628e56794e503cf7af32561
makiable <makiable@gmail.com>
2015-08-11 22:42:24 +0900

    first
    test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	public class UIScripts : MonoBehaviour {
+		
+		// Use this for initialization
+		void OnButtonDown(string type){
+			if (type == "Start") {
+				Application.LoadLevel("Test_Game");
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/UIScripts.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void OnButtonDown(string type){
+		if (type == "Start") {
+			Application.LoadLevel("Test_Game");
+		}
+	}
+}
