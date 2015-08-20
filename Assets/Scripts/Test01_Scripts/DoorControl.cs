3cfee5809f511108c2725d6d229f44c118ddca94
makiable <makiable@gmail.com>
2015-08-20 21:57:24 +0900

    Change Code 8_20
    문 애니메이션 까지 완료

---
+++ Assets/Scripts/Test01_Scripts/DoorControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour {
+
+	//컨트롤할 게임 메니저.
+	public Test_01 mGameManager;
+
+	//애니메이터 컨트롤 변수.
+	private Animator mAnimator;
+
+	// 상태 정의 
+	public enum Status{
+		Idle,
+		Open,
+		Close
+	}
+
+	// 애니메이션만 세팅 하면 된다.
+
+
+	//트리거로 입력 받았을때 오픈 애니메이션을 보여준다. 0.5초 동안 
+
+	// Use this for initialization
+	void Start () {
+		mAnimator = gameObject.GetComponent<Animator> ();
+		Debug.Log (mAnimator);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetStatus(Status status){
+
+		switch (status) {
+			case Status.Open:
+				mAnimator.SetTrigger ("Open");
+
+			Debug.Log("Send Open Massage");
+
+				break;
+				
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Test01_Scripts/Test_01.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test_01 : MonoBehaviour {
+
+	// 0 찾기 게임..
+
+	public DoorControl BlackDoorControl;
+	public DoorControl WhiteDoorControl;
+
+
+	public int a = 0;
+	public int b = 0;
+
+	public TextMesh mTextMash;
+
+	public enum Status{
+		ChooseBlack,
+		ChooseWhite
+	}
+
+	IEnumerator delayTime(){
+		yield return new WaitForSeconds (5);
+		Debug.Log ("Time = " + Time.time);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		a = Random.Range (0, 2);
+
+		if (a == 0) {
+			b = 1;
+		} else
+			b = 0;
+
+		Debug.Log ("a="+ a);
+		Debug.Log ("b="+ b);
+
+		mTextMash.text = LiveDays.LiveDaysint.ToString();
+
+	}
+	void OnButtonDown(string type){
+		Debug.Log("OnButtonDown");
+
+		if (type == "0") {
+
+			BlackDoorControl.SetStatus(DoorControl.Status.Open);
+
+			Debug.Log("Open Black door__type="+type);
+
+
+			if (a == 0) {
+				Invoke("Sucess",1);
+			}
+			else
+				Invoke("Fail",1);
+		}
+
+		if (type == "1") {
+
+			WhiteDoorControl.SetStatus(DoorControl.Status.Open);
+
+			Debug.Log("Open White door__typetype="+type);
+
+			//yield return new WaitForSeconds(1.2f);
+
+			if (b == 0) {
+				Invoke("Sucess",1);
+			}
+			else
+				Invoke("Fail",1);
+		}
+	}
+
+	void Sucess(){
+		Application.LoadLevel("Test_Success");
+		Debug.Log("Success");
+	}
+
+	void Fail(){
+		Application.LoadLevel("Test_Fail");
+		Debug.Log ("Fail");
+	}
+	
+}
